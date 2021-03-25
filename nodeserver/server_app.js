4787550d5020610966dc28138878ee85219d1319
Nathan Bradshaw <nathansbradshaw@gmail.com>
2021-03-24 18:47:41 -0600

    add side bar

--- nodeserver/server_app.js
+++ nodeserver/server_app.js
@@ -21,41 +21,57 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
 let client = 0;
+
+
 io.on("connection", (socket) => {
   //------------------------------------------------------- New user connection
   connnections.push(socket);
   socket.username = "Anonymous-" + client++;
+  users.push(socket.username);
+
   console.log("a user connected, " + socket.username);
-  // Tell client that he is connected
-  socket.emit("connection", (username, ack) => {
-    console.log('Sending username')
-    ack({ status: "ok" });
-    username({ username: socket.username });
-  });
+  // Tell all clients that a client has connected to chat
+  const usernames = JSON.stringify(users)
+  socket.emit("your username", socket.username)
+  io.emit("connection to chat", socket.username, usernames );
+  
+
   //-------------------------------------------------------- handle disconnection
   socket.on("disconnect", () => {
     //Remove user from list if disconnected
-    const index = connnections.indexOf(socket.username);
+    const index = users.indexOf(socket.username);
     if (index > -1) {
-      connnections.splice(index, 1);
-      console.log(connnections)
+      users.splice(index, 1);
+      console.log(users)
     }
-    console.log("user disconnected");
+    const usernames = JSON.stringify(users)
+    // Tell everyone that a user disconnected
+    io.emit("disconnection from chat", socket.username, usernames);
   });
+
   //-------------------------------------------------------- Chat, send back ack with Username and status
-  socket.on("chat", (message, ack,) => {
+  socket.on("chat", (message, ack) => {
     ack({ username: socket.username,
     status: 'ok'});
     io.emit("chat", message, socket.username
     );
   });
+
   // ------------------------------------------------------- Change Username
   socket.on("change username", (message, ack) => {
+    const index = users.indexOf(socket.username);
+    if (index > -1) {
+      users[index] = message;
+    }
+    const usernames = JSON.stringify(users)
     const oldname = socket.username
     socket.username = message
     ack({ username: socket.username,
       status: 'ok'});
-    io.emit("change username report", (`${oldname} changed their name to ${socket.username}` ));
+    // Tell everyone except the user that changed their name, that there was a name change
+    socket.broadcast.emit("change username report", (`${oldname} changed their name to ${socket.username}` ), usernames);
+    // Tell users that we succesfully changed their name.
+    socket.emit("change username report", (`Your name has been changed to ${socket.username}`), usernames)
   });
 });
 
--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     const changeusername = document.getElementById('username-submit')
     const Input = document.getElementById('chat-input')
     const chatwindow = document.getElementById('chat-window')
+    const userList = document.getElementById('user-list')
 
 
     // ------------------------------------------------ Sending chat message
@@ -37,6 +38,19 @@ const App = () => {
       }
     });
 
+    const usersList = (usernames) => {
+      userList.innerHTML = `<h2 class='has-text-white'> Connected Users </h2>`
+      usernames.forEach(element => {
+        if (element === username) {
+          userList.innerHTML += `<p class='has-text-white'> (${element}) - YOU </p>`
+
+        } else {
+          userList.innerHTML += `<p class='has-text-white'> ${element} </p>`
+        }
+
+      });
+    }
+
     chat.addEventListener('click', sendMessage)
     chatEnter.addEventListener('click', sendMessage)
 
@@ -59,13 +73,41 @@ const App = () => {
 
     })
     // ------------------------------------------------- Display name changes
-    socket.on('change username report', message => {
+    socket.on('your username', message => {
+      username = message;
+    })
+    // ------------------------------------------------- Display name changes
+    socket.on('change username report', (message, users) => {
+      let usersArray = JSON.parse(users);
+     usersList(usersArray);
+      chatwindow.innerHTML += `
+          <div class="is-flex is-align-content-center is-justify-content-center my-1">
+          <div class="is-align-center">
+            <p>From Server: ${message} </p>
+          </div>
+        </div>`
+    })
+    // ------------------------------------------------- TEll users of new connections
+    socket.on('connection to chat', (username, users) => {
+      let usersArray = JSON.parse(users);
+     usersList(usersArray);
       chatwindow.innerHTML += `
       <div class="is-flex is-align-content-center is-justify-content-center my-1">
       <div class="is-align-center">
-        <p>From Me: ${message} </p>
+        <p>From Server: ${username} Connected </p>
       </div>
     </div>`
+    })
+    // ------------------------------------------------- TEll users of disconnections
+    socket.on('disconnection from chat', (username, users) => {
+      let usersArray = JSON.parse(users);
+      usersList(usersArray);
+      chatwindow.innerHTML += `
+          <div class="is-flex is-align-content-center is-justify-content-center my-1">
+          <div class="is-align-center">
+            <p>From Server: ${username} disconnected </p>
+          </div>
+        </div>`
     })
     // ------------------------------------------------- Display chat message
     socket.on('chat', (msg, user) => {
@@ -81,7 +123,6 @@ const App = () => {
         `
       } else {
         chatwindow.innerHTML += `
-        
         <div class="is-flex is-align-content-flex-start is-justify-content-flex-start my-1">
         <div class="is-align-content-start box has-background-info">
           <p class="has-text-white">From ${user}: ${msg} </p>
@@ -96,28 +137,36 @@ const App = () => {
     [])
   //In my school we had hardly any tech related courses but I found a lot of joy in creating things with programming so I taught 
   return (
-    <div id="container is-max-desktop">
+    <div id="container is-max-desktop site">
       <Nav />
-      <div className='mt-3 container' id="chatSection">
+      <div className='mt-3 container ' id="chatSection">
+        <div class="tile is-ancestor">
+          <div class="tile is-10 is-vertical is-parent" id="chat-window">
 
 
-        <div className="mx-6 " id="chat-window"></div>
-
-        <div className="field is-horizontal mt-4">
-          <div className="field-label is-normal">
-            <label className="label">Enter message</label>
           </div>
-          <div className="field-body">
-            <div className="field  has-addons">
-              <div className="control">
-                <input className="input" type="text" placeholder="Enter your message here" id='chat-input' />
-                <input type="submit" className='hidden-button' id='chat_enter' />
-              </div>
-              <div className="control">
-                <input className='button' type="submit" id="chat-submit" value="enter" />
-                <input className='button' type="submit" id="username-submit" value="change Username" />
-              </div>
+          <div class="tile is-4 is-parent has-background-info">
 
+            <div className="mx-6 site-content content-window" id="user-list"></div>
+          </div>
+        </div>
+        <div className='footer1'>
+          <div className="field is-horizontal  mt-4 ">
+            <div className="field-label is-normal">
+              <label className="label">Enter message</label>
+            </div>
+            <div className="field-body">
+              <div className="field  has-addons">
+                <div className="control">
+                  <input className="input" type="text" placeholder="Enter your message here" id='chat-input' />
+                  <input type="submit" className='hidden-button' id='chat_enter' />
+                </div>
+                <div className="control">
+                  <input className='button is-info' type="submit " id="chat-submit" value="enter" />
+                  <input className='button' type="submit" id="username-submit" value="change Username" />
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
